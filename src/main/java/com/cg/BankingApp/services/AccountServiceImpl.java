24f95fa756314ea07bfe1d1f433d2474652329a7
nandakg <nandakg@inlen8520080268.corp.capgemini.com>
2023-10-05 10:49:53 +0530

    Intial commit add

--- src/main/java/com/cg/BankingApp/services/AccountServiceImpl.java
+++ src/main/java/com/cg/BankingApp/services/AccountServiceImpl.java
@@ -1,5 +1,66 @@
 package com.cg.BankingApp.services;
 
-public class AccountServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cg.BankingApp.models.Account;
+import com.cg.BankingApp.repositorys.AccountRepository;
+import com.cg.BankingApp.repositorys.CustomerRepository;
+
+public class AccountServiceImpl implements AccountService{
+
+	@Autowired
+	private AccountRepository accountRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private TransactionService transactionService;
+	@Override
+	
+	public Account addAccount(Account account) {
+		// TODO Auto-generated method stub
+	
+		return null;
+	}
+	@Override
+	public boolean deleteAccount(int accountNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Account getAccountDetails(Long accountNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Account> getAllAccountDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public double withdraw(long accountNumber, double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double deposit(long accountNumber, double amount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double getBalance(long accountNUmber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public boolean fundTransfer(long accountNumberFrom, long accountNumberTo, double amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void updateAccount(Account account) {
+		// TODO Auto-generated method stub
+		
+	}
 }
